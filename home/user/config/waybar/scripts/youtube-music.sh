0251a08a55b234e663538ee1ec45090a723df47f
Owen <owen.debiasio@gmail.com>
2026-01-13 17:14:11 -0500

    Update icons in youtube-music.sh script

--- home/user/config/waybar/scripts/youtube-music.sh
+++ home/user/config/waybar/scripts/youtube-music.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+
 player="chromium"
 
-title=$(playerctl -p $player metadata title 2>/dev/null)
-artist=$(playerctl -p $player metadata artist 2>/dev/null)
+title=$(playerctl -p "$player" metadata title 2>/dev/null)
+artist=$(playerctl -p "$player" metadata artist 2>/dev/null)
 
 if [ -n "$title" ]; then
-    echo "      $artist - $title  |"
+    echo "  $artist - $title |"
 else
-    echo "  "
+    echo " "
 fi
